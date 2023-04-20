c877208652ccf340a0a05980c2fe5aa3d1345398
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2023-04-20 16:06:52 -0500

    create data load file

---
+++ load_data.py
@@ -0,0 +1,21 @@
+# Load and clean data
+
+import pandas as pd
+import numpy as np
+
+
+def load_train_data()
+    '''
+    '''
+    # Load
+    df = pd.read_csv("Kaggle_download/train.csv")
+
+    # Define household and individual variables
+    ind_vars = []
+    hh_vars = []
+
+    # Clean
+
+    # Impute?
+
+    return df
