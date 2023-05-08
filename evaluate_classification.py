e157b42fd42f5ed3830a2ca6bde74fec2a4e6dc5
bentoviml <bentovim@uchicago.edu>
2023-05-07 20:12:08 -0500

    edited eval_class

--- evaluate_classification.py
+++ evaluate_classification.py
@@ -1,17 +1,21 @@
 # Helper function to evaluate ML model classifications
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, recall_score, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, recall_score
 
-def evaluate_classification(y_pred, y_true, labels=[1,2,3,4], cm = False):
+def evaluate_classification(y_pred, y_true, labels=[1,2,3,4], cm=False, return_vals=False):
     '''
     COMPLETE DOC STRING
     '''
     accuracy = accuracy_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred, average="weighted")
-    recall = recall_score(y_true, y_pred, average="weighted")
-    print("Accuracy:", accuracy)
-    print("F1 Score:", f1)
-    print("Recall:", recall)
+    recall = recall_score(y_true, y_pred, average=None)
+    print("Accuracy:", accuracy.round(2))
+    print("F1 Score:", f1.round(2))
+    print("Recall:", 'Label 1:', recall[0].round(2), 'Label 2:', recall[1].round(2), 
+          'Label 3:', recall[2].round(2), 'Label 4:', recall[3].round(2))
     if cm is True:
         cm = confusion_matrix(y_true, y_pred, labels=labels)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
         disp.plot()
+    
+    if return_vals:
+        return accuracy, f1, recall
