011e5b682b32101e87a046a4d48246c56e5aa95b
klinkoberstar <111700741+klinkoberstar@users.noreply.github.com>
2023-05-17 00:08:13 -0500

    updates to naive bayes

--- evaluate_classification.py
+++ evaluate_classification.py
@@ -6,14 +6,16 @@ def evaluate_classification(y_pred, y_true, l=[1,2,3,4], cm=False, return_vals=F
     COMPLETE DOC STRING
     '''
     eval_dict = {}
-    eval_dict['accuracy'] = accuracy_score(y_true, y_pred)
-    eval_dict['f1'] = f1_score(y_true, y_pred, average="weighted")
-    eval_dict['recall'] = recall_score(y_true, y_pred, average=None)
-    print("Accuracy:", eval_dict['accuracy'].round(2))
-    print("F1 Score:", eval_dict['f1'].round(2))
-    print("Recall:", 'Label 1:', eval_dict['recall'][0].round(2), 'Label 2:', 
-          eval_dict['recall'][1].round(2), 'Label 3:', eval_dict['recall'][2].round(2), 
-          'Label 4:', eval_dict['recall'][3].round(2))
+    eval_dict['Accuracy'] = accuracy_score(y_true, y_pred)
+    eval_dict['Weighted F1'] = f1_score(y_true, y_pred, average="weighted")
+    eval_dict['Macro F1'] = f1_score(y_true, y_pred, average="macro")
+    eval_dict['Recall'] = recall_score(y_true, y_pred, average=None)
+    print("Accuracy:", eval_dict['Accuracy'].round(2))
+    print("Weighted F1 Score:", eval_dict['Weighted F1'].round(2))
+    print("Macro F1 Score:", eval_dict['Macro F1'].round(2))
+    print("Recall:", 'Label 1:', eval_dict['Recall'][0].round(2), 'Label 2:', 
+          eval_dict['Recall'][1].round(2), 'Label 3:', eval_dict['Recall'][2].round(2), 
+          'Label 4:', eval_dict['Recall'][3].round(2))
     if cm is True:
         cm = confusion_matrix(y_true, y_pred, labels=l)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=l)
