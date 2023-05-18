507ab4e59687bd5008ec164b7a521a611f7acc78
klinkoberstar <111700741+klinkoberstar@users.noreply.github.com>
2023-05-18 18:06:35 -0500

    Update evaluate_classification.py

--- evaluate_classification.py
+++ evaluate_classification.py
@@ -12,8 +12,9 @@ def evaluate_classification(y_pred, y_true, l=[1,2,3,4], cm=False, return_vals=F
     eval_dict['recall'] = recall_score(y_true, y_pred, average=None)
     eval_dict['precision'] = recall_score(y_true, y_pred, average=None)
     print("Accuracy:", eval_dict['accuracy'].round(2))
-    print("F1 Score:", eval_dict['f1'].round(2))
+    print("Weighted F1 Score:", eval_dict['f1'].round(2))
     print("Macro F1 Score:", eval_dict['Macro f1'].round(2))
+    print("Precision:", eval_dict['precision'].round(2))
     print("Recall:", 'Label 1:', eval_dict['recall'][0].round(2), 'Label 2:', 
           eval_dict['recall'][1].round(2))
     if len(eval_dict['recall']) == 4:
