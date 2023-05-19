2aacd0c234a0b24a26e4f9c230f1fd3d67b80dd3
klinkoberstar <111700741+klinkoberstar@users.noreply.github.com>
2023-05-18 19:13:04 -0500

    Update evaluate_classification.py

--- evaluate_classification.py
+++ evaluate_classification.py
@@ -9,21 +9,31 @@ def evaluate_classification(y_pred, y_true, l=[1,2,3,4], cm=False, return_vals=F
     eval_dict['accuracy'] = accuracy_score(y_true, y_pred)
     eval_dict['f1'] = f1_score(y_true, y_pred, average="weighted")
     eval_dict['Macro f1'] = f1_score(y_true, y_pred, average="macro")
-    eval_dict['recall'] = recall_score(y_true, y_pred, average=None)
-    eval_dict['precision'] = recall_score(y_true, y_pred, average=None)
-    print("Accuracy:", eval_dict['accuracy'].round(2))
-    print("Weighted F1 Score:", eval_dict['f1'].round(2))
-    print("Macro F1 Score:", eval_dict['Macro f1'].round(2))
-    print("Precision:", eval_dict['precision'].round(2))
-    print("Recall:", 'Label 1:', eval_dict['recall'][0].round(2), 'Label 2:', 
-          eval_dict['recall'][1].round(2))
-    if len(eval_dict['recall']) == 4:
-          print('      Label 3:', eval_dict['recall'][2].round(2))
-          print('      Label 4:', eval_dict['recall'][3].round(2))
+    eval_dict['recall'] = recall_score(y_true, y_pred, average= None)
+    eval_dict['precision'] = precision_score(y_true, y_pred, average= None, zero_division=1)
+    print("Accuracy:", eval_dict['accuracy'].round(2),"\n")
+    print("Weighted F1 Score:", eval_dict['f1'].round(2),"\n")
+    print("Macro F1 Score:", eval_dict['Macro f1'].round(2),"\n")
+    if len(eval_dict['precision']) == 2:
+        print("Precision:", 'Experiencing Poverty:', eval_dict['precision'][1].round(2), 'Not Experiencing Poverty:', 
+            eval_dict['precision'][0].round(2),"\n")
+    else:
+        print("Precision:", 'Class 1:', eval_dict['precision'][0].round(2), 'Class 2:', 
+            eval_dict['precision'][1].round(2), ' Class 3:', eval_dict['precision'][2].round(2))
+        if len(eval_dict['precision']) == 4:
+            print('           Class 4:', eval_dict['precision'][3].round(2),"\n")
+    
+    if len(eval_dict['recall']) == 2:
+        print("Recall:", 'Experiencing Poverty:', eval_dict['recall'][1].round(2), 'Not Experiencing Poverty:', 
+            eval_dict['recall'][0].round(2),"\n")
+    else:
+        print("Recall:", 'Class 1:', eval_dict['recall'][0].round(2), 'Class 2:', 
+            eval_dict['recall'][1].round(2), ' Class 3:', eval_dict['recall'][2].round(2))
+        if len(eval_dict['recall']) == 4:
+            print('        Class 4:', eval_dict['recall'][3].round(2),"\n")
     if cm is True:
         cm = confusion_matrix(y_true, y_pred, labels=l)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=l)
         disp.plot()
-    
     if return_vals:
         return eval_dict
